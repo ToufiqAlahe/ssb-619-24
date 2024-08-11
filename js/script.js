6f717ac142915428aca703ad36f72469edc89947
ToufiqAlahe <164491805+ToufiqAlahe@users.noreply.github.com>
2024-08-11 20:01:56 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+/* ex-1*/ 
+function showText(){
+	document.getElementById('s_text').innerHTML="Hello SSB";
+}
+
+// ex2
+function showDate2(){
+	document.getElementById('s_date').innerHTML=Date();
+}
+// ex3
+function click3(){
+	document.getElementById('demo').innerHTML="Hello JavaScript!";
+}
+// ex4
+function bulbOn4(){
+	document.getElementById('img4').src="images/bulbon.gif";
+}
+function bulbOff4(){
+	document.getElementById('img4').src="images/bulboff.gif";
+}
+// ex5
+function bulbOn5(){
+	document.getElementById('img5').src="images/bulbon.gif";
+}
+function bulbOff5(){
+	document.getElementById('img5').src="images/bulboff.gif";
+}
+// ex6
+function bulbOn6(){
+	document.getElementById('img6').src="images/bulbon.gif";
+}
+function bulbOff6(){
+	document.getElementById('img6').src="images/bulboff.gif";
+}
+// ex7
+function bulbOn7(){
+	document.getElementById('img7').src="images/bulbon.gif";
+}
+function bulbOff7(){
+	document.getElementById('img7').src="images/bulboff.gif";
+}
+// ex8
+function bulbOn8(){
+	document.getElementById('img8').src="images/bulbon.gif";
+}
+function bulbOff8(){
+	document.getElementById('img8').src="images/bulboff.gif";
+}
+// ex9
+function styleChange9(){
+	document.getElementById('css9').style.fontSize='30px';
+}
+// ex10
+function styleChange10(){
+	document.getElementById('css10').style.color='blue';
+}
+// ex11
+function styleChange11(){
+	document.getElementById('css11').style.display='none';
+}
+// ex12
+function styleChange12(){
+	window.print('css12');
+}
