ff14dcecacb928adf680e61b3f66499633af34ac
ToufiqAlahe <164491805+ToufiqAlahe@users.noreply.github.com>
2024-08-21 15:14:49 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -198,3 +198,115 @@ let _100 = 500;
  function myResult33(){
  document.getElementById("demo33").innerHTML = _x + _100;
 }
+// ex34
+let a = 5;
+let x34 = (100 + 50) * a;
+function myResult34(){
+	document.getElementById('demo34').innerHTML = x34;
+}
+// ex35
+let num7, num8;
+num7 = 76;
+num8 = 89;
+num7 -= num8;
+function myResult35(){
+	document.getElementById('demo35').innerHTML = num7;
+}
+// ex36
+let num9, num10;
+num9 = 90;
+num10 = 76;
+num9 *= num10;
+function myResult36(){
+	document.getElementById('demo36').innerHTML = num9;
+}
+// ex37
+let num11, num12;
+num11 = 90;
+num12 = 76;
+num11 /= num12;
+function myResult37(){
+	document.getElementById('demo37').innerHTML = num11;
+}
+// ex38
+let num13, num14;
+num13 = 92;
+num14 = 7;
+num13 %= num14;
+function myResult38(){
+	document.getElementById('demo38').innerHTML = num13;
+}
+// ex39
+let num15, num16;
+num15 = 2;
+num16 = 7;
+num15 **= num16;
+function myResult39(){
+	document.getElementById('demo39').innerHTML = num15;
+}
+// ex40
+let text1 = "what a very ";
+text1 += "nice day";
+function myResult40(){
+	document.getElementById('demo40').innerHTML =text1;
+}
+// ex41
+let x41 = 5 + 5;
+let y41 = "5" + 5;
+let z41 = "Hello" + 5;
+function myResult41(){
+	document.getElementById('demo41').innerHTML = x41 +  "<br>" + y41 + "<br>" + z41;
+}
+// ex42
+let numb1, numb2, result42;
+numb1 = 9;
+numb2 = 4;
+result42 = numb1 > numb2;
+function myResult42(){
+	document.getElementById('demo42').innerHTML = result42;
+}
+// ex43
+let numb3, numb4, result43;
+numb1 = 10;
+numb2 = "10";
+result43 = numb1 === numb2;
+function myResult43(){
+	document.getElementById('demo43').innerHTML = result43;
+}
+// ex44
+let y44 = 123e5;
+let z44 = 123e-5;
+function myResult44(){
+	document.getElementById('demo44').innerHTML = y44 + "<br>" + z44;
+}
+// ex45
+let x45 = 5;
+let y45 = 5;
+let z45 = 6;
+function myResult45(){
+  document.getElementById("demo45").innerHTML = (x45 == y45) + "<br>" + (x45 == z45);
+}
+// ex46
+function myResult46(){
+  document.getElementById("demo46").innerHTML = typeof 10 + "<br>" +
+  typeof "John" + "<br>" + typeof true;
+}
+// ex47
+const car47 = ["Saab", "Volvo", "BMW"];
+function myResult47(){
+	document.getElementById('demo47').innerHTML = car47 [2];
+}
+// ex48
+const car48 = ["Saab", "Volvo", "BMW", 10, true];
+function myResult48(){
+	document.getElementById('demo48').innerHTML = car48 [0] + "<br>" + car48[3] + "<br>" + car48[4];
+}
+// ex49
+function myResult49(){
+	document.getElementsByClassName('demo49')[2].innerHTML= "<h2>He is the 1st Mughal emperor</h2>";
+}
+// ex50
+function myResult50(){
+	var info = document.getElementsByTagName('h2');
+	alert(info.length);
+}
